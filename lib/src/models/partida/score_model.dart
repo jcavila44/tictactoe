575d6a97e46c52ea2deab83bf229cea2ef826966
Jose Carlos <50499480+jcavila44@users.noreply.github.com>
2021-02-25 20:23:34 -0500

    Update puntos model
    Update puntos model

--- lib/src/models/partida/score_model.dart
+++ lib/src/models/partida/score_model.dart
@@ -1,6 +1,6 @@
 import 'package:tictactoe/src/models/jugador/jugador_model.dart';
 import 'package:tictactoe/src/models/partida/partida_model.dart';
-import 'package:tictactoe/src/models/puntos/puntos.dart';
+import 'package:tictactoe/src/models/utils/puntos.dart';
 
 class Score {
   int id;
