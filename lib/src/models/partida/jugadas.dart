e4049b9dee184f5e3dd924f9e0aed2970d9c12e2
delbasto <delbastoangel@hotmail.com>
2021-02-25 20:39:46 -0500

    add

--- lib/src/models/partida/jugadas.dart
+++ lib/src/models/partida/jugadas.dart
@@ -1,22 +1,25 @@
-class jugadas {
-  int id_jugadas;
-  int id_partida;
-  int id_jugador;
+import '../jugador/jugador_model.dart';
+import 'partida_model.dart';
+
+class Jugadas {
+  int id;
+  Partida partida;
+  Jugador jugador;
   String posicion;
   int turno;
 
 
-  jugadas({this.id_jugadas, this.id_partida, this.id_jugador, this.posicion, this.turno});
+  Jugadas({this.id, this.partida, this.jugador, this.posicion, this.turno});
 
-  factory jugadas.fromJson(Map<String, dynamic> parsedJson) {
-    return jugadas(
-        id_jugadas: parsedJson['id_jugadas'], id_partida: parsedJson['id_partida'], id_jugador: parsedJson['id_jugador'], posicion: parsedJson['posicion'], turno: parsedJson['turno']);
+  factory Jugadas.fromJson(Map<String, dynamic> parsedJson) {
+    return Jugadas(
+        id: parsedJson['id'], partida: Partida.fromJson(parsedJson['partida']), jugador: Jugador.fromJson(parsedJson['jugador']), posicion: parsedJson['posicion'], turno: parsedJson['turno']);
   }
 
   Map<String, dynamic> toJson() => {
-        'id_jugadas': id_jugadas,
-        'id_partida': id_partida,
-        'id_jugador': id_jugador,
+        'id': id,
+        'partida': partida,
+        'jugador': jugador,
         'posicion': posicion,
         'turno': turno,
       };
