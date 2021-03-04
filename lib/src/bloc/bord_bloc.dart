dec25d16fd4bdfa1f2df4a530ee9cbe5ed51ca0f
jordanZapata <jordan199442@gmail.com>
2021-03-04 18:21:04 -0500

    Implement thel block

---
+++ lib/src/bloc/bord_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'dart:math';
+import '../bloc/helpers.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'bord_event.dart';
+part 'bord_state.dart';
+
+class BordBloc extends Bloc<BordEvent, BordState> {
+  List<int> _bordState = [55, 55, 55, 55, 55, 55, 55, 55, 55];
+  bool playerOneTurn = true;
+  int currentMove = 0;
+  int winner;
+
+  List<int> get getBordState => _bordState;
+
+  @override
+  BordInitial get initialState => BordInitial();
+
+  @override
+  Stream<BordState> mapEventToState(
+    BordEvent event,
+  ) async* {
+    if (event is Tap) {
+      currentMove++;
+      updateBord(event.index);
+      winner = checkWinner(_bordState);
+      if (winner != null) {
+        yield GameResult(_bordState, result: winner);
+      } else
+        yield UpdateBord(bordState: _bordState);
+    }
+  }
+
+  void updateBord(int index) {
+    if (_bordState[index] != 0 && _bordState[index] != 1) {
+      _bordState[index] = 1;
+      playerOneTurn = true;
+    }
+  }
+}
