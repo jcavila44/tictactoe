66532062c82ada2250d006c43a11d4a390702b98
alejandromagno1 <alejandromagno12358@gmail.com>
2021-03-11 17:57:04 -0500

    actualización test

--- test/users_test.dart
+++ test/users_test.dart
@@ -2,11 +2,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tictactoe/bloc/bord_bloc.dart';
 
 void main() {
-  test('Prueba unitaria', () {
-    final counter = BordBloc();
+  group('Players', () {
+    test('Test un jugador', () {
+      final player = BordBloc();
 
-    counter.updateBord(1);
+      player.updateBord(0);
 
-    expect(counter.playerOneTurn, true);
+      expect(player.playerOneTurn, true);
+    });
+
+    test('Test dos jugadores', () {
+      final player = BordBloc();
+
+      player.updateBord(2);
+
+      expect(player.playerOneTurn, false);
+    });
   });
 }
